5b99ae336b4706bb15e1a9016f22f5f5c5e02afb
Muhammad Omer <omer@neusol.com>
2022-09-30 03:24:27 +0500

    > some minor updates.

--- src/redux/storeConfig/store.js
+++ src/redux/storeConfig/store.js
@@ -3,13 +3,13 @@ import createDebounce from "redux-debounced"
 import thunk from "redux-thunk"
 import rootReducer from "../reducers/rootReducer"
 
-const middlewares = [thunk, createDebounce()]
+const middleWares = [thunk, createDebounce()]
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = legacy_createStore(
   rootReducer,
   {},
-  composeEnhancers(applyMiddleware(...middlewares))
+  composeEnhancers(applyMiddleware(...middleWares))
 )
 
-export { store }
+export { store };
